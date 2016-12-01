a9054467b8855fe686c325f2bbed8049b7000342
Jose Manuel Perez <jperezg86@gmail.com>
2016-12-01 17:16:41 -0600

    Se crea el proyecto y se sube

---
+++ LuvItFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LuvItFeed
+//
+//  Created by Jose Manuel Perez on 12/1/16.
+//  Copyright © 2016 LuvIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'LuvItFeed' do
+	pod 'Alamofire', :git => 'https://github.com/Alamofire/Alamofire.git', :branch => 'master'
+end
