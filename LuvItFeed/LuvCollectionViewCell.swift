32057bb470e64eb745f1e714225d5f1f5ac39ae1
Jose Manuel Perez <jperezg86@gmail.com>
2016-12-05 17:16:19 -0600

    Se suben cosas

---
+++ LuvItFeed/LuvCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LuvCollectionViewCell.swift
+//  LuvItFeed
+//
+//  Created by Jose Manuel Perez on 12/5/16.
+//  Copyright © 2016 LuvIT. All rights reserved.
+//
+
+import UIKit
+
+class LuvCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
